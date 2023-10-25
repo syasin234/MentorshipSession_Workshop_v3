d6c84ccaf995fa3227d78c6716ca5d1af8ef9218
syasin234 <70025579+syasin234@users.noreply.github.com>
2023-10-25 10:22:53 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,25 @@
+import 'dotenv/config';
 import { defineConfig } from "vite";
-import { getMapsOptimizers, getMapsScripts } from "wa-map-optimizer-vite";
+import { getMaps, getMapsOptimizers, getMapsScripts, LogLevel, OptimizeOptions } from "wa-map-optimizer-vite";
+
+const maps = getMaps();
+
+let optimizerOptions: OptimizeOptions = {
+    logs: process.env.LOG_LEVEL && process.env.LOG_LEVEL in LogLevel ? LogLevel[process.env.LOG_LEVEL] : LogLevel.NORMAL,
+};
+
+if (process.env.TILESET_OPTIMIZATION && process.env.TILESET_OPTIMIZATION === "true") {
+    const qualityMin = process.env.TILESET_OPTIMIZATION_QUALITY_MIN ? parseInt(process.env.TILESET_OPTIMIZATION_QUALITY_MIN) : 0.9;
+    const qualityMax = process.env.TILESET_OPTIMIZATION_QUALITY_MAX ? parseInt(process.env.TILESET_OPTIMIZATION_QUALITY_MAX) : 1;
+
+    optimizerOptions.output = {
+        tileset: {
+            compress: {
+                quality: [qualityMin, qualityMax],
+            }
+        }
+    }
+}
 
 export default defineConfig({
     base: "./",
@@ -7,11 +27,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
-                ...getMapsScripts("./maps"),
+                ...getMapsScripts(maps),
             },
         },
     },
-    plugins: [...getMapsOptimizers(undefined, "./maps")],
+    plugins: [...getMapsOptimizers(maps, optimizerOptions)],
     server: {
         host: "localhost",
         headers: {
